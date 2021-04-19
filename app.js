00e5e9f4f99361970cc2a620beb1ba7816a3d52b
101061877 <cameron.custer@mines.sdsmt.edu>
2021-04-19 16:28:22 -0600

    updated for get endpoint

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-var express = require('express');
+const express = require('express');
 
-var app = express();
+const app = express();
+const port = process.env.PORT || 3000;
+const figurineRouter = express.Router();
 
-var port = process.env.PORT || 3000;
+figurineRouter.route('/figurine')
+    .get((req, res) => {
+        const response = {hello: 'This is the GET endpoint'};
 
-app.get('/', (req, res)=>{
+    res.json(response);
+});
+
+app.use('/api', figurineRouter);
+
+app.get('/', (req, res) => {
     res.send('Welcome to our Node Mon API');
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('Running on port ' + port);
 });
